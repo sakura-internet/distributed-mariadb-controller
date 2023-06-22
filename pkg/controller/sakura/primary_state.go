731cfffef3d6dd2340715a0aea52c11607e1884a
Drumato <y-sugawara@sakura.ad.jp>
2023-06-22 01:44:43 +0000

    shrink nftables rules that only accepts inbound from sakura
    Signed-off-by: Drumato <y-sugawara@sakura.ad.jp>

--- pkg/controller/sakura/primary_state.go
+++ pkg/controller/sakura/primary_state.go
@@ -62,7 +62,7 @@ func (c *SAKURAController) triggerRunOnStateChangesToPrimary() error {
 	// [STEP1]: END of setting MariaDB state
 
 	// [STEP2]: START of setting nftables state
-	if err := c.acceptTCP3306TrafficFromSakuraPrefixes(); err != nil {
+	if err := c.acceptTCP3306Traffic(); err != nil {
 		return err
 	}
 	// [STEP2]: END of setting nftables state
@@ -97,31 +97,18 @@ func (c *SAKURAController) triggerRunOnStateKeepsPrimary() error {
 	return nil
 }
 
-// acceptTCP3306TrafficFromSakuraPrefixes sets the rule that accepts the inbound communication from the SAKURA hosts.
-func (c *SAKURAController) acceptTCP3306TrafficFromSakuraPrefixes() error {
-	const (
-		sakuraSrcAddrGroupName = "@sakura_addrs"
-	)
+// acceptTCP3306Traffic sets the rule that accepts the inbound communication.
+func (c *SAKURAController) acceptTCP3306Traffic() error {
 	if err := c.nftablesConnector.FlushChain(nftables.BuiltinTableFilter, nftablesMariaDBChain); err != nil {
 		return err
 	}
 
 	acceptMatches := []nftables.Match{
 		nftables.IFNameMatch(mariaDBServerDefaultIFName),
-		nftables.IPSrcAddrMatch(sakuraSrcAddrGroupName),
 		nftables.TCPDstPortMatch(mariaDBServerDefaultPort),
 	}
 
 	if err := c.nftablesConnector.AddRule(nftables.BuiltinTableFilter, nftablesMariaDBChain, acceptMatches, nftables.AcceptStatement()); err != nil {
-		return err
-	}
-
-	rejectMatches := []nftables.Match{
-		nftables.IFNameMatch(mariaDBServerDefaultIFName),
-		nftables.TCPDstPortMatch(mariaDBServerDefaultPort),
-	}
-
-	if err := c.nftablesConnector.AddRule(nftables.BuiltinTableFilter, nftablesMariaDBChain, rejectMatches, nftables.RejectStatement()); err != nil {
 		return err
 	}
 
